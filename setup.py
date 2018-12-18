61667c586481151ce0b254551dbefa1d7c6046c3
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2018-12-18 19:40:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -14,11 +14,12 @@ setup(
   author = 'Yugant Hadiyal',                   # Type in your name
   author_email = 'hadiyalyugant@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/YugantM',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/YugantM/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/YugantM/textcleaner/archive/V0.1.tar.gz',    # I explain this later on
   keywords = ['MEANINGFULL', 'NLP', 'DATASCIENCE','DATACLEANING','PREPROCESSING'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+          'NLTK',
+          're',
+          'string'
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
