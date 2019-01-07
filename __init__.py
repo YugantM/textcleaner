e2d9e3cc477fea51dbc6fadbae853d68b9eaeda1
Yugant M. Hadiyal <hadiyalyugant@gmail.com>
2019-01-07 13:40:05 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,14 +1,9 @@
-
-# coding: utf-8
-
-# In[ ]:
-
-
+from textcleaner.textcleaner import document
 from textcleaner.textcleaner import clear_blank_lines
 from textcleaner.textcleaner import strip_all
 from textcleaner.textcleaner import lower_all
 from textcleaner.textcleaner import remove_numbers
-from textcleaner.textcleaner import remove_symblos
+from textcleaner.textcleaner import remove_symbols
 from textcleaner.textcleaner import remove_stpwrds
 from textcleaner.textcleaner import token_it
 from textcleaner.textcleaner import stemming
